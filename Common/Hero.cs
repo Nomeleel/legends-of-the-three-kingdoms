acbc5d62ee3ebbcba393227bf90963efb1549bbe
Nomeleel <choonlay@outlook.com>
2019-05-09 23:11:11 +0800

    first

---
+++ Common/Hero.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common
+{
+    public class Hero
+    {
+        private string name;
+        private Kingdoms kingdoms;
+        private int magatama;
+        private string skill;
+
+
+        // 掉血 被动技能 重写
+        public int Magatama
+        {
+            set
+            {
+                if (value < magatama)
+                {
+                    // 委托掉血 被动技能
+                }
+                magatama = value;
+            }
+            get
+            {
+                return magatama;
+            }
+        }
+
+        public void SetSkill()
+        {
+
+        }
+
+        // skill  技能目标 附体
+
+    }
+}
---
+++ Common/Kingdoms.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common
+{
+    public enum Kingdoms
+    {
+        Wei,
+        Shu,
+        Wu,
+        Qun
+    }
+}
